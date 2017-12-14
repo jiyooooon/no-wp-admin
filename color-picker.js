82866228618f385b3df82721e4fa27755f09d419
Julie Kim <kimjulie@live.com>
2017-12-14 13:47:24 -0500

    Delete color-picker.js

--- color-picker.js
+++
@@ -1,7 +0,0 @@
-(function($) {
-    // Add Color Picker to all inputs that have 'color-field' class
-    $(function() {
-        $('.jkwhmhh_color_1').wpColorPicker();
-        $('.jkwhmhh_color_2').wpColorPicker();
-    });
-})(jQuery);
