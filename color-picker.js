1993573109c9db92fb5576efe669474b4ba749ce
Julie Kim <kimjulie@live.com>
2017-12-14 13:45:42 -0500

    Add files via upload

---
+++ color-picker.js
@@ -0,0 +1,7 @@
+(function($) {
+    // Add Color Picker to all inputs that have 'color-field' class
+    $(function() {
+        $('.jkwhmhh_color_1').wpColorPicker();
+        $('.jkwhmhh_color_2').wpColorPicker();
+    });
+})(jQuery);
